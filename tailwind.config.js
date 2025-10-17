81d75aef05f82155fa5579453fc7fdf48036d1cb
aguang5241 <gliu4@wpi.edu>
2025-10-17 18:10:47 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   README.md
    	new file:   app-1.png
    	new file:   app-2.png
    	new file:   app-3.png
    	new file:   app-4.png
    	new file:   app-5.png
    	new file:   app-5.png:Zone.Identifier
    	new file:   app-6.png
    	new file:   app-6.png:Zone.Identifier
    	new file:   app-7.png:Zone.Identifier
    	modified:   eslint.config.js
    	modified:   index.html
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   postcss.config.js
    	new file:   public/app-1.png
    	new file:   public/app-2.png
    	new file:   public/app-3.png
    	new file:   public/app-4.png
    	new file:   public/app-5.png
    	new file:   public/app-6.png
    	new file:   public/logo.png
    	modified:   src/App.jsx
    	new file:   src/assets/FaceQ.png
    	modified:   src/assets/react.svg
    	modified:   src/index.css
    	modified:   src/main.jsx
    	new file:   tailwind.config.js
    	modified:   vite.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundSize: {
+        '200%': '200%',
+      },
+      animation: {
+        'gradient-x': 'gradient-x 8s ease infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
