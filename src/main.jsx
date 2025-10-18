9ae858174a4837036711d68ec51bf5282f48a00d
aguang5241 <gliu4@wpi.edu>
2025-10-18 16:18:50 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Publications from './pages/Publications.jsx'
 import CV from './pages/CV.jsx'
 import Contacts from './pages/Contacts.jsx'
 
+// When deploying to GitHub Pages it's simpler to use HashRouter so
+// client-side routes work without a server rewrite. URLs will look like
+// /my-portfolio/#/cv
 const basename = import.meta.env.BASE_URL
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/cv" element={<CV />} />
         <Route path="/publications" element={<Publications />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
